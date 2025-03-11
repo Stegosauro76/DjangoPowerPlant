7d3504625a637ede66026424d750c75f5bcf10fb
Lorenzo <l.meloccaro@intac.it>
2025-03-11 17:11:03 +0100

    fix bug ajax

--- gestione_macchine/gestione/admin.py
+++ gestione_macchine/gestione/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Stabilimento,Macchinario,WorkHour
+from .models import * 
 
-admin.site.register(Stabilimento)
+
+
+admin.site.register(Stabilimento)      
 admin.site.register(Macchinario)
 admin.site.register(WorkHour)
